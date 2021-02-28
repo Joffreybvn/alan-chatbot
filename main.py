d50701230a667ad4158a692924ab4a0e8ce54257
Joffrey Bienvenu <joffreybvn@gmail.com>
2021-02-28 21:54:33 +0100

    Switched to 0.0.0.0

--- main.py
+++ main.py
@@ -111,4 +111,4 @@ app.router.add_post("/api/messages", messages)
 
 if __name__ == "__main__":
 
-    web.run_app(app, host="localhost", port=config.PORT)
+    web.run_app(app, host="0.0.0.0", port=config.PORT)
